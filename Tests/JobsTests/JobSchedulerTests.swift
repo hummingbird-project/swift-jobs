5fb1eae1e0ae5487902199109b9987d5581845c1
Adam Fowler <adamfowler71@gmail.com>
2024-07-08 12:48:55 +0000

    Use ISO8601DateFormatter so tests work on Linux

--- Tests/JobsTests/JobSchedulerTests.swift
+++ Tests/JobsTests/JobSchedulerTests.swift
@@ -19,44 +19,48 @@ import XCTest
 
 final class JobSchedulerTests: XCTestCase {
     func testSchedule(start: String, expectedEnd: String, schedule: Schedule) throws {
-        let startDate = try Date.ISO8601FormatStyle.iso8601(timeZone: schedule.calendar.timeZone).parse(start)
-        let expectedEndDate = try Date.ISO8601FormatStyle.iso8601(timeZone: schedule.calendar.timeZone).parse(expectedEnd)
+        let iso8601Formatter = ISO8601DateFormatter()
+        guard let startDate = iso8601Formatter.date(from: start),
+            let expectedEndDate = iso8601Formatter.date(from: expectedEnd) else {
+                XCTFail("Failed to parse dates")
+                return
+            }
         let end = schedule.nextDate(after: startDate)
         XCTAssertEqual(expectedEndDate, end)
     }
 
     func testMinuteSchedule() throws {
-        try self.testSchedule(start: "2021-06-21T21:10:15", expectedEnd: "2021-06-21T21:10:43", schedule: .everyMinute(second: 43))
-        try self.testSchedule(start: "1999-12-31T23:59:25", expectedEnd: "2000-01-01T00:00:15", schedule: .everyMinute(second: 15))
+        try self.testSchedule(start: "2021-06-21T21:10:15Z", expectedEnd: "2021-06-21T21:10:43Z", schedule: .everyMinute(second: 43))
+        try self.testSchedule(start: "1999-12-31T23:59:25Z", expectedEnd: "2000-01-01T00:00:15Z", schedule: .everyMinute(second: 15))
     }
 
     func testHourlySchedule() throws {
-        try self.testSchedule(start: "2021-06-21T21:10:15", expectedEnd: "2021-06-21T21:58:00", schedule: .hourly(minute: 58))
-        try self.testSchedule(start: "1999-12-31T23:59:25", expectedEnd: "2000-01-01T00:01:00", schedule: .hourly(minute: 1))
+        try self.testSchedule(start: "2021-06-21T21:10:15Z", expectedEnd: "2021-06-21T21:58:00Z", schedule: .hourly(minute: 58))
+        try self.testSchedule(start: "1999-12-31T23:59:25Z", expectedEnd: "2000-01-01T00:01:00Z", schedule: .hourly(minute: 1))
     }
 
     func testDailySchedule() throws {
-        try self.testSchedule(start: "2021-06-21T21:10:15", expectedEnd: "2021-06-22T01:15:00", schedule: .daily(hour: 1, minute: 15, timeZone: .gmt))
-        try self.testSchedule(start: "1999-12-31T23:59:25", expectedEnd: "2000-01-01T06:15:00", schedule: .daily(hour: 6, minute: 15))
-        try self.testSchedule(start: "2024-02-28T23:59:25", expectedEnd: "2024-02-29T06:15:00", schedule: .daily(hour: 6, minute: 15))
+        try self.testSchedule(start: "2021-06-21T21:10:15Z", expectedEnd: "2021-06-22T01:15:00Z", schedule: .daily(hour: 1, minute: 15, timeZone: .init(secondsFromGMT: 0)!))
+        try self.testSchedule(start: "1999-12-31T23:59:25Z", expectedEnd: "2000-01-01T06:15:00Z", schedule: .daily(hour: 6, minute: 15))
+        try self.testSchedule(start: "2024-02-28T23:59:25Z", expectedEnd: "2024-02-29T06:15:00Z", schedule: .daily(hour: 6, minute: 15))
     }
 
     func testWeeklySchedule() throws {
-        try self.testSchedule(start: "2021-06-21T21:10:15", expectedEnd: "2021-06-27T04:00:00", schedule: .weekly(day: .sunday, hour: 4))
-        try self.testSchedule(start: "2024-03-19T23:59:56", expectedEnd: "2024-03-24T04:00:00", schedule: .weekly(day: .sunday, hour: 4, timeZone: .gmt))
-        try self.testSchedule(start: "2024-03-19T23:59:56", expectedEnd: "2024-03-24T04:00:00", schedule: .weekly(day: .sunday, hour: 4))
-        try self.testSchedule(start: "1999-12-31T23:59:25", expectedEnd: "2000-01-01T08:00:00", schedule: .weekly(day: .saturday, hour: 8))
+        try self.testSchedule(start: "2021-06-21T21:10:15Z", expectedEnd: "2021-06-27T04:00:00Z", schedule: .weekly(day: .sunday, hour: 4))
+        try self.testSchedule(start: "2024-03-19T23:59:56Z", expectedEnd: "2024-03-24T04:00:00Z", schedule: .weekly(day: .sunday, hour: 4, timeZone: .init(secondsFromGMT: 0)!))
+        try self.testSchedule(start: "2024-03-19T23:59:56Z", expectedEnd: "2024-03-24T04:00:00Z", schedule: .weekly(day: .sunday, hour: 4))
+        try self.testSchedule(start: "1999-12-31T23:59:25Z", expectedEnd: "2000-01-01T08:00:00Z", schedule: .weekly(day: .saturday, hour: 8))
     }
 
     func testMonthlySchedule() throws {
-        try self.testSchedule(start: "2021-06-21T21:10:15", expectedEnd: "2021-07-14T04:00:00", schedule: .monthly(date: 14, hour: 4))
-        try self.testSchedule(start: "2024-03-19T23:59:56", expectedEnd: "2024-04-14T04:00:00", schedule: .monthly(date: 14, hour: 4))
-        try self.testSchedule(start: "1999-12-31T23:59:25", expectedEnd: "2000-01-14T04:00:00", schedule: .monthly(date: 14, hour: 4))
+        try self.testSchedule(start: "2021-06-21T21:10:15Z", expectedEnd: "2021-07-14T04:00:00Z", schedule: .monthly(date: 14, hour: 4))
+        try self.testSchedule(start: "2024-03-19T23:59:56Z", expectedEnd: "2024-04-14T04:00:00Z", schedule: .monthly(date: 14, hour: 4))
+        try self.testSchedule(start: "1999-12-31T23:59:25Z", expectedEnd: "2000-01-14T04:00:00Z", schedule: .monthly(date: 14, hour: 4))
     }
 
     func testScheduleTimeZone() throws {
-        let startDate = try Date.ISO8601FormatStyle.iso8601(timeZone: .current).parse("2021-06-21T21:10:15")
-        let scheduledDate = try XCTUnwrap(Schedule.daily(hour: 4, timeZone: .gmt).nextDate(after: startDate))
+        let startDate = ISO8601DateFormatter().date(from: "2021-06-21T21:10:15Z")!
+        let scheduledDate = try XCTUnwrap(Schedule.daily(hour: 4, timeZone: .init(secondsFromGMT: 0)!).nextDate(after: startDate))
         let dateComponents = Calendar.current.dateComponents([.hour], from: scheduledDate)
         // check timezone difference is the same as the difference in the schedule
         XCTAssertEqual(TimeZone.current.secondsFromGMT(), (dateComponents.hour! - 4) * 3600)
@@ -75,7 +79,7 @@ final class JobSchedulerTests: XCTestCase {
         try schedule.addJob(Job1(), schedule: .hourly(minute: 30))
         try schedule.addJob(Job2(), schedule: .daily(hour: 4))
 
-        let startDate = try Date.ISO8601FormatStyle.iso8601(timeZone: .current).parse("2024-04-14T02:00:00")
+        let startDate = ISO8601DateFormatter().date(from: "2024-04-14T02:00:00Z")!
         for jobIndex in schedule.startIndex..<schedule.endIndex {
             schedule[jobIndex].nextScheduledDate = schedule[jobIndex].schedule.nextDate(after: startDate)!
         }
