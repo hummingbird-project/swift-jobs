6c870d693114265db7750c69e91eecfce3e7433b
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2024-09-14 21:14:29 -0400

    moved queue size to include job name

--- Sources/Jobs/JobQueue.swift
+++ Sources/Jobs/JobQueue.swift
@@ -48,9 +48,9 @@ public struct JobQueue<Queue: JobQueueDriver>: Service {
         options: JobOptions = .init()
     ) async throws -> Queue.JobID {
         let buffer = try self.queue.encode(id: id, parameters: parameters)
-        Meter(label: "swift.jobs.meter", dimensions: [("status", "queued")]).increment()
         let jobName = id.name
         let id = try await self.queue.push(buffer, options: options)
+        Meter(label: "swift.jobs.meter", dimensions: [("status", "queued"), ("name", jobName)]).increment()
         self.logger.debug(
             "Pushed Job",
             metadata: ["JobID": .stringConvertible(id), "JobName": .string(jobName)]
