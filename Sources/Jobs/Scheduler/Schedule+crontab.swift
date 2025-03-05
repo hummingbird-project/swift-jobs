6640273369bc2f6c7dc32124057b7b4e28a12ce5
Adam Fowler <adamfowler71@gmail.com>
2025-03-05 08:16:18 +0000

    crontab schedule (#74)

---
+++ Sources/Jobs/Scheduler/Schedule+crontab.swift
@@ -0,0 +1,197 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2025 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import RegexBuilder
+
+#if os(Linux)
+@preconcurrency import Foundation
+#else
+import Foundation
+#endif
+
+extension Schedule {
+    ///  Create Scehdule using crontab style string
+    /// - Parameters:
+    ///   - crontab: Crontab string
+    ///   - timeZone: Timezone to run schedule in
+    /// - Throws: ScheduleError for corrupt crontabs and crontabs we don't suppoty
+    static func crontab(_ crontab: String, timeZone: TimeZone = .current) throws -> Self {
+        if crontab.first == "@" {
+            guard let crontab = Self.crontabNicknames[crontab] else { throw ScheduleError("Unrecognised crontab nick name \(crontab)") }
+            return try Self.crontab(crontab, timeZone: timeZone)
+        }
+        let values = crontab.split(separator: " ", omittingEmptySubsequences: true)
+        guard values.count == 5 else { throw ScheduleError("Crontab string requires 5 values") }
+        let minutes = try Self.parse(values[0], range: 0...59, count: 60)
+        let hours = try Self.parse(values[1], range: 0...23, count: 24)
+        let dayOfMonth = try Self.parse(values[2], range: 1...31, count: 31)
+        let month = try Self.parse(values[3], range: 1...12, count: 12) {
+            if let month = Int($0) { return month }
+            if let month = Self.monthNameNumberMap[$0] { return month }
+            return nil
+        }.map {
+            guard let month = Month(rawValue: $0) else { throw ScheduleError("Invalid month value") }
+            return month
+        }
+        let dayOfWeek = try Self.parse(values[4], range: 0...7, count: 7) {
+            if let day = Int($0) { return day }
+            if let day = Self.dayNameNumberMap[$0] { return day }
+            return nil
+        }.map {
+            guard let day = Day(rawValue: $0 + 1) else {
+                if $0 == 7 {
+                    return Day.sunday
+                }
+                throw ScheduleError("Invalid day value")
+            }
+            return day
+        }.sorted()
+
+        let schedule = Self(second: .specific(0), minute: minutes, hour: hours, date: dayOfMonth, month: month, day: dayOfWeek, timeZone: timeZone)
+
+        // if we have a selection set for either day or date we don't support setting the other value
+        switch (schedule.day, schedule.date) {
+        case (.selection, .specific), (.selection, .selection), (.specific, .selection):
+            throw ScheduleError(
+                "Schedule does not support a combination of date and weekday where one is a selection of values and the other is not the wildcard '*'"
+            )
+        default:
+            break
+        }
+        return schedule
+    }
+
+    static func parse(
+        _ entry: Substring,
+        range: ClosedRange<Int>,
+        count: Int,
+        transform: (Substring) -> Int? = { Int($0) }
+    ) throws -> Parameter<Int> {
+        let numberRegex = Regex {
+            Anchor.startOfLine
+            Capture {
+                OneOrMore(.word)
+            }
+            Anchor.endOfLine
+        }
+        let rangeRegex = Regex {
+            Anchor.startOfLine
+            Capture {
+                OneOrMore(.word)
+            }
+            "-"
+            Capture {
+                OneOrMore(.word)
+            }
+            Anchor.endOfLine
+        }
+        let everyRegex = Regex {
+            Anchor.startOfLine
+            "*/"
+            Capture {
+                OneOrMore(.digit)
+            } transform: {
+                Int($0)!
+            }
+            Anchor.endOfLine
+        }
+        let everyInRangeRegex = Regex {
+            Anchor.startOfLine
+            Capture {
+                OneOrMore(.word)
+            }
+            "-"
+            Capture {
+                OneOrMore(.word)
+            }
+            "/"
+            Capture {
+                OneOrMore(.digit)
+            } transform: {
+                Int($0)!
+            }
+            Anchor.endOfLine
+        }
+        let selectionRegex: Regex = Regex {
+            Anchor.startOfLine
+            Capture {
+                OneOrMore {
+                    OneOrMore(.word)
+                    ","
+                }
+                OneOrMore(.word)
+            }
+            Anchor.endOfLine
+        }
+        if entry == "*" { return .any }
+        if let values = try? numberRegex.wholeMatch(in: entry), let value = transform(values.1) {
+            return .init(value)
+        } else if let values = try? rangeRegex.wholeMatch(in: entry), let lower = transform(values.1), let upper = transform(values.2) {
+            guard lower < upper else { throw ScheduleError("Crontab range requires first value to be before the second: \(values.0)") }
+            return .init(lower...upper)
+        } else if let values = try? everyRegex.wholeMatch(in: entry) {
+            let numberOfValues = (count + values.1 - 1) / values.1
+            let array = (0..<numberOfValues).map { range.lowerBound + $0 * values.1 }
+            return .init(array)
+        } else if let values = try? everyInRangeRegex.wholeMatch(in: entry), let lower = transform(values.1), let upper = transform(values.2) {
+            guard lower < upper else { throw ScheduleError("Crontab range requires first value to be before the second: \(values.0)") }
+            let range = lower...upper
+            let numberOfValues = (range.count + values.3 - 1) / values.3
+            let array = (0..<numberOfValues).map { range.lowerBound + $0 * values.3 }
+            return .init(array)
+        } else if let values = try? selectionRegex.wholeMatch(in: entry) {
+            let values = try values.1.split(separator: ",").map {
+                guard let value = transform($0) else { throw ScheduleError("Unrecognised values \($0)") }
+                return value
+            }
+            return .init(values)
+        } else {
+            throw ScheduleError("Unrecognised crontab element \(entry)")
+        }
+    }
+
+    private static let crontabNicknames: [String: String] = [
+        "@yearly": "0 0 1 1 *",
+        "@annually": "0 0 1 1 *",
+        "@monthly": "0 0 1 * *",
+        "@weekly": "0 0 * * 0",
+        "@daily": "0 0 * * *",
+        "@hourly": "0 * * * *",
+    ]
+
+    private static let dayNameNumberMap: [Substring: Int] = [
+        "sun": 0,
+        "mon": 1,
+        "tue": 2,
+        "wed": 3,
+        "thu": 4,
+        "fri": 5,
+        "sat": 6,
+    ]
+
+    private static let monthNameNumberMap: [Substring: Int] = [
+        "jan": 1,
+        "feb": 2,
+        "mar": 3,
+        "apr": 4,
+        "may": 5,
+        "jun": 6,
+        "jul": 7,
+        "aug": 8,
+        "sep": 9,
+        "oct": 10,
+        "nov": 11,
+        "dec": 12,
+    ]
+}
---
+++ Tests/JobsTests/ScheduleTests.swift
@@ -0,0 +1,212 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2025 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+
+@testable import Jobs
+
+final class ScheduleTests: XCTestCase {
+    func testCrontabNumbers() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("5 12 20 4 *"),
+            Schedule(minute: 5, hour: 12, date: 20, month: .init(.april), day: .any)
+        )
+    }
+    func testCrontabDays() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("0 12 * * 0"),
+            Schedule(minute: 0, hour: 12, date: .any, month: .any, day: .init(.sunday))
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("0 12 * * 7"),
+            Schedule(minute: 0, hour: 12, date: .any, month: .any, day: .init(.sunday))
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("0 12 * * 4"),
+            Schedule(minute: 0, hour: 12, date: .any, month: .any, day: .init(.thursday))
+        )
+    }
+    func testCrontabRanges() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("5 12-14 20 4 *"),
+            Schedule(minute: 5, hour: [12, 13, 14], date: 20, month: .init(.april), day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("5 12 20 6-9 *"),
+            Schedule(
+                minute: 5,
+                hour: 12,
+                date: 20,
+                month: [.june, .july, .august, .september],
+                day: .any
+            )
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("5 12 * * 0-2"),
+            Schedule(minute: 5, hour: 12, date: .any, month: .any, day: [.sunday, .monday, .tuesday])
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("5 12 * * 6-7"),
+            Schedule(minute: 5, hour: 12, date: .any, month: .any, day: [.saturday, .sunday])
+        )
+    }
+    func testCrontabEvery() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("5 */6 * * *"),
+            Schedule(minute: 5, hour: [0, 6, 12, 18], date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("5 */5 * * *"),
+            Schedule(minute: 5, hour: [0, 5, 10, 15, 20], date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("15 10 * * */2"),
+            Schedule(minute: 15, hour: 10, date: .any, month: .any, day: [.sunday, .tuesday, .thursday, .saturday])
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("15 10 */6 * *"),
+            Schedule(minute: 15, hour: 10, date: [1, 7, 13, 19, 25, 31], month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("15 10 1 */3 *"),
+            Schedule(minute: 15, hour: 10, date: 1, month: [.january, .april, .july, .october], day: .any)
+        )
+    }
+    func testCrontabEveryInRange() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("5 3-15/6 * * *"),
+            Schedule(minute: 5, hour: [3, 9, 15], date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("15 10 * * 1-6/2"),
+            Schedule(minute: 15, hour: 10, date: .any, month: .any, day: [.monday, .wednesday, .friday])
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("15 10 1 2-12/3 *"),
+            Schedule(minute: 15, hour: 10, date: 1, month: [.february, .may, .august, .november], day: .any)
+        )
+    }
+    func testCrontabSelection() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("5 8,16 * * *"),
+            Schedule(minute: 5, hour: [8, 16], date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("5,10 6 * * *"),
+            Schedule(minute: [5, 10], hour: 6, date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("15 10 2 1,6,9 *"),
+            Schedule(minute: 15, hour: 10, date: 2, month: [.january, .june, .september], day: .any)
+        )
+    }
+    func testFixedSelections() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("* 8 * * *"),
+            Schedule(minute: .init(Array(0..<60)), hour: 8, date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("* * * 6 *"),
+            Schedule(
+                minute: .init(0..<60),
+                hour: .init(0..<24),
+                date: .any,
+                month: .init(.june),
+                day: .init([.sunday, .monday, .tuesday, .wednesday, .thursday, .friday, .saturday])
+            )
+        )
+    }
+    func testDayNames() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("0 8 * * mon"),
+            Schedule(minute: 0, hour: 8, date: .any, month: .any, day: .init(.monday))
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("0 19 * 6 tue-sat"),
+            Schedule(
+                minute: 0,
+                hour: 19,
+                date: .any,
+                month: .init(.june),
+                day: .init([.tuesday, .wednesday, .thursday, .friday, .saturday])
+            )
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("0 19 * 6 sun,wed,fri"),
+            Schedule(
+                minute: 0,
+                hour: 19,
+                date: .any,
+                month: .init(.june),
+                day: .init([.sunday, .wednesday, .friday])
+            )
+        )
+        XCTAssertThrowsError(try Schedule.crontab("0 19 * 6 sun,jan,fri"))
+    }
+    func testMonthNames() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("0 8 * jan *"),
+            Schedule(minute: 0, hour: 8, date: .any, month: .init(.january), day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("0 19 * feb-may *"),
+            Schedule(
+                minute: 0,
+                hour: 19,
+                date: .any,
+                month: [.february, .march, .april, .may],
+                day: .any
+            )
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("0 19 * jun,jul,aug *"),
+            Schedule(
+                minute: 0,
+                hour: 19,
+                date: .any,
+                month: [.june, .july, .august],
+                day: .any
+            )
+        )
+        XCTAssertThrowsError(try Schedule.crontab("0 19 * jan-sat *"))
+        XCTAssertThrowsError(try Schedule.crontab("0 19 * may-feb"))
+    }
+    func testNicknames() throws {
+        XCTAssertEqual(
+            try Schedule.crontab("@hourly"),
+            Schedule(minute: 0, hour: .any, date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("@daily"),
+            Schedule(minute: 0, hour: 0, date: .any, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("@weekly"),
+            Schedule(minute: 0, hour: 0, date: .any, month: .any, day: .init(.sunday))
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("@monthly"),
+            Schedule(minute: 0, hour: 0, date: 1, month: .any, day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("@yearly"),
+            Schedule(minute: 0, hour: 0, date: 1, month: .init(.january), day: .any)
+        )
+        XCTAssertEqual(
+            try Schedule.crontab("@annually"),
+            Schedule(minute: 0, hour: 0, date: 1, month: .init(.january), day: .any)
+        )
+        XCTAssertThrowsError(try Schedule.crontab("@unrecognised"))
+    }
+}
