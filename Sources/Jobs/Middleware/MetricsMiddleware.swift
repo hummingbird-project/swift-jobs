4edbef806b9f4e3f0b112d6fac71de65fd2b8269
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2025-03-06 07:58:05 -0500

    Adding Job name in metrics (#72)
    * Adding Job name in metrics
    
    * Added job name to metrics
    * Added job name to tracing
    
    * swift format
    
    * Cleaned up job metrics
    
    * swift format
    
    * more job metrics
    
    * clean up
    
    * PR comments
    
    * swift format

--- Sources/Jobs/Middleware/MetricsMiddleware.swift
+++ Sources/Jobs/Middleware/MetricsMiddleware.swift
@@ -62,7 +62,8 @@ public struct MetricsJobMiddleware: JobMiddleware {
         Meter(
             label: Self.meterLabel,
             dimensions: [
-                ("status", JobStatus.queued.rawValue)
+                ("status", JobStatus.queued.rawValue),
+                ("name", Parameters.jobName),
             ]
         ).increment()
     }
@@ -74,24 +75,41 @@ public struct MetricsJobMiddleware: JobMiddleware {
     ///   - jobInstanceID: Job instance identifer
     @inlinable
     public func onPopJob(result: Result<any JobInstanceProtocol, JobQueueError>, jobInstanceID: String) async {
-        // Decrement the current queue by 1
-        Meter(
-            label: Self.meterLabel,
-            dimensions: [
-                ("status", JobStatus.queued.rawValue)
-            ]
-        ).decrement()
 
         switch result {
         case .failure(let error):
+            var counterDimensions: [(String, String)] = [
+                ("reason", error.code.description)
+            ]
+
+            if let jobName = error.jobName {
+                let jobNameDimension = ("name", jobName)
+                counterDimensions.append(jobNameDimension)
+
+                Meter(
+                    label: Self.meterLabel,
+                    dimensions: [
+                        ("status", JobStatus.queued.rawValue),
+                        jobNameDimension,
+                    ]
+                ).decrement()
+            }
+
             Counter(
                 label: Self.discardedCounter,
-                dimensions: [
-                    ("reason", error.code.description)
-                ]
+                dimensions: counterDimensions
             ).increment()
 
         case .success(let job):
+            // Decrement the current queue by 1
+            Meter(
+                label: Self.meterLabel,
+                dimensions: [
+                    ("status", JobStatus.queued.rawValue),
+                    ("name", job.name),
+                ]
+            ).decrement()
+
             // Calculate wait time from queued to processing
             let jobQueuedDuration = Date.now.timeIntervalSince(job.queuedAt)
             Timer(
@@ -119,14 +137,16 @@ public struct MetricsJobMiddleware: JobMiddleware {
         Meter(
             label: Self.meterLabel,
             dimensions: [
-                ("status", JobStatus.processing.rawValue)
+                ("status", JobStatus.processing.rawValue),
+                ("name", job.name),
             ]
         ).increment()
         defer {
             Meter(
                 label: Self.meterLabel,
                 dimensions: [
-                    ("status", JobStatus.processing.rawValue)
+                    ("status", JobStatus.processing.rawValue),
+                    ("name", job.name),
                 ]
             ).decrement()
         }
@@ -154,7 +174,8 @@ public struct MetricsJobMiddleware: JobMiddleware {
                 Meter(
                     label: Self.meterLabel,
                     dimensions: [
-                        ("status", JobStatus.queued.rawValue)
+                        ("status", JobStatus.queued.rawValue),
+                        ("name", job.name),
                     ]
                 ).increment()
 
