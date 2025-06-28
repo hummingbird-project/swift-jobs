77923b55bfde28df629c4a5fafa8d5b7c0c94579
Adam Fowler <adamfowler71@gmail.com>
2025-06-28 12:57:16 +0100

    Documentation fix ups

--- Sources/Jobs/JobName.swift
+++ Sources/Jobs/JobName.swift
@@ -12,17 +12,29 @@
 //
 //===----------------------------------------------------------------------===//
 
+/// Identifier for a Job.
+///
+/// This should be unique as it is used to identify jobs when deserializing them.
+///
+/// `JobName` has an associated generic parameter to indicate the parameters associated with this job
 public struct JobName<Parameters: Codable>: CustomStringConvertible, ExpressibleByStringLiteral, Sendable, Equatable {
     public let name: String
 
+    /// Initialize JobName
+    /// - Parameters:
+    ///   - string: Job name
+    ///   - parameters: Parameters associated with job
     public init(_ string: String, parameters: Parameters.Type = Parameters.self) {
         self.name = string
     }
 
+    ///  Initialize JobName with string literal
+    /// - Parameter string: job name
     public init(stringLiteral string: String) {
         self.name = string
     }
 
+    /// JobName textual representation
     public var description: String {
         self.name
     }
